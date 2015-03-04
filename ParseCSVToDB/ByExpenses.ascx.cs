52293393b65cc79d2d759e577930df692e6ae79a
Ming Chien <mingch@yahoo.com>
2015-03-04 13:31:48 -0800

    allow ByExpenses to be call from other pages as detail page

--- ParseCSVToDB/ByExpenses.ascx.cs
+++ ParseCSVToDB/ByExpenses.ascx.cs
@@ -34,7 +34,7 @@ namespace ParseCSVToDB
             //sortImage.ImageUrl = "../images/view_sort_descending.png";
             //GridView1.HeaderRow.Cells[7].Controls.Add(sortImage);
 
-            litTitle.Text = "Top " + ddlTopN.Text + " expenses with the higest amount (" + (this.Page as dynamic).selectedDateRangeText + ")";
+            //litTitle.Text = "Top " + ddlTopN.Text + " expenses with the higest amount (" + (this.Page as dynamic).selectedDateRangeText + ")";
         }      
         
         int _cnt1 = 0;
@@ -62,10 +62,23 @@ namespace ParseCSVToDB
         
         private void BindGrid(string strOrderBy)
         {
+            string strAndClause = string.Empty;
+            if (Request.QueryString["name"] != null)
+            {
+                strAndClause += "AND name=" + Common.CommonLib.Quote(Request.QueryString["name"].ToString());
+                litTitle.Text = "Top " + ddlTopN.Text + " expenses by " + Request.QueryString["name"].ToString() + " (" + (this.Page as dynamic).selectedDateRangeText + ")";
+            }
+                
+            else if (Request.QueryString["ministry"] != null)
+            {
+                strAndClause += "AND ministry=" + Common.CommonLib.Quote(Request.QueryString["ministry"].ToString());
+                litTitle.Text = "Top " + ddlTopN.Text + " expenses by " + Request.QueryString["ministry"].ToString() + " (" + (this.Page as dynamic).selectedDateRangeText + ")";
+            }
+                
             strOrderBy = (strOrderBy == string.Empty) ? "decAmount DESC" : strOrderBy.Replace("Amount", "decAmount").Replace("DateIncurred", "DTDateIncurred");
             var strSQL = lblSQL.Text = "SELECT TOP " + ddlTopN.SelectedValue + " Ministry,Position,Name,Category,Type,DateIncurred,DTDateIncurred,Amount,decAmount,Description,Receipt" +
                                        " FROM [dbo].[goa_expenses] " +
-                                       " WHERE " + (this.Page as dynamic).selectedDateRangeValue +
+                                       " WHERE " + (this.Page as dynamic).selectedDateRangeValue + strAndClause +
                                        " ORDER BY " + strOrderBy;           
             dataTable = Common.CommonLib.fillDataTable(ConfigurationManager.ConnectionStrings["ApplicationServices"].ConnectionString, strSQL);
             GridView1.DataSource = dataTable;
--- ParseCSVToDB/ByOfficial.ascx.cs
+++ ParseCSVToDB/ByOfficial.ascx.cs
@@ -9,30 +9,16 @@ namespace ParseCSVToDB
     {
         protected void Page_Load(object sender, EventArgs e)
         {
-            //if (!IsPostBack)
-            {
-                /*
-                var strTop = Request.QueryString["top"] != null ? Request.QueryString["top"].ToString() : "25";
-                try
-                {
-                    ddlTopN.SelectedValue = litTopN.Text = strTop;
-                }
-                catch
-                {
-                    ddlTopN.SelectedIndex = 0;
-                }
-                */
-                var strSQL = lblSQL.Text = "SELECT TOP " + ddlTopN.SelectedValue + " Ministry, SUM(decAmount) AS Total, COUNT(*) As Cnt, Name, SUM(decAmount)/COUNT(*) As Average " +
-                                            "FROM [dbo].[goa_expenses] " +
-                                            "WHERE decAmount > 0 AND " + (this.Page as dynamic).selectedDateRangeValue +
-                                            "GROUP BY Name, Ministry " +
-                                            "ORDER BY SUM(decamount) DESC ";
-                SqlDataSource1.ConnectionString = ConfigurationManager.ConnectionStrings["ApplicationServices"].ConnectionString;
-                SqlDataSource1.SelectCommand = strSQL;
-                GridView1.DataBind();
+            var strSQL = lblSQL.Text = "SELECT TOP " + ddlTopN.SelectedValue + " Ministry, SUM(decAmount) AS Total, COUNT(*) As Cnt, Name, SUM(decAmount)/COUNT(*) As Average " +
+                                        "FROM [dbo].[goa_expenses] " +
+                                        "WHERE decAmount > 0 AND " + (this.Page as dynamic).selectedDateRangeValue +
+                                        "GROUP BY Name, Ministry " +
+                                        "ORDER BY SUM(decamount) DESC ";
+            SqlDataSource1.ConnectionString = ConfigurationManager.ConnectionStrings["ApplicationServices"].ConnectionString;
+            SqlDataSource1.SelectCommand = strSQL;
+            GridView1.DataBind();
 
-                litTitle.Text = "Top " + ddlTopN.Text + " officials with the higest total expenses (" + (this.Page as dynamic).selectedDateRangeText + ")";
-            }
+            litTitle.Text = "Top " + ddlTopN.Text + " officials with the higest total expenses (" + (this.Page as dynamic).selectedDateRangeText + ")";
         }
 
         int _cnt1 = 0;
@@ -48,6 +34,9 @@ namespace ParseCSVToDB
             {
                 e.Row.Cells[0].Text = (++_cnt1).ToString() + ".";
                 DataRow row = ((DataRowView)e.Row.DataItem).Row;
+                e.Row.Cells[1].Text = (++_cnt1).ToString() + ".";
+                e.Row.Cells[1].Text = "<a title='Click to pop-open a new browser to view the details' target='_blank' href='/Analyze1?v=2&name=" + 
+                                                                            Server.UrlEncode(row[3].ToString()) + "'>" + row[3].ToString() + "</a>";
                 String strTotal = row[1].ToString();
                 String strCnt = row[2].ToString();
                 //e.Row.Cells[5].Text = "$" + (Convert.ToDecimal(strTotal) / Convert.ToInt32(strCnt)).ToString("N2");
